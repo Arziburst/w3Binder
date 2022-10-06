9c95cabea534f792736029361ea2231f3cb73f4e
Belartale <belartale@gmail.com>
2022-10-06 14:20:15 +0300

    replaces icons for units and add buildings for Undead

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,54 +1,98 @@
 // unitImportJsonMarker
-import FrostWyrm from './FrostWyrm.json';
-import ObsidianStatue from './ObsidianStatue.json';
-import MeatWagon from './MeatWagon.json';
-import Necromancer from './Necromancer.json';
-import Banshee from './Banshee.json';
-import Abomination from './Abomination.json';
-import Shade from './Shade.json';
-import CryptFiend from './CryptFiend.json';
-import Ghoul from './Ghoul.json';
-import Dreadlord from './Dreadlord.json';
-import Lich from './Lich.json';
-import DeathKnight from './DeathKnight.json';
-import Shaman from './Shaman.json';
-import Acolyte from './Acolyte.json';
-import Wisp from './Wisp.json';
-import Peon from './Peon.json';
-import Peasant from './Peasant.json';
+
+// Heroes Orc
 import Blademaster from './Blademaster.json';
-import TaurenChieftain from './TaurenChieftain.json';
-import ShadowHunter from './ShadowHunter.json';
 import FarSeer from './FarSeer.json';
-import Headhunter from './Headhunter.json';
+import ShadowHunter from './ShadowHunter.json';
+import TaurenChieftain from './TaurenChieftain.json';
+// Heroes Undead
+import DeathKnight from './DeathKnight.json';
+import Lich from './Lich.json';
+import Dreadlord from './Dreadlord.json';
+import CryptLord from './CryptLord.json';
+
+// Mobs Human
+import Peasant from './Peasant.json';
+// Mobs Orc
+import Peon from './Peon.json';
 import Grunt from './Grunt.json';
+import Headhunter from './Headhunter.json';
+import Shaman from './Shaman.json';
+// Mobs Undead
+import Acolyte from './Acolyte.json';
+import Shade from './Shade.json';
+import Ghoul from './Ghoul.json';
+import Abomination from './Abomination.json';
+import MeatWagon from './MeatWagon.json';
+import CryptFiend from './CryptFiend.json';
+import Gargoyle from './Gargoyle.json';
+import Banshee from './Banshee.json';
+import Necromancer from './Necromancer.json';
+import ObsidianStatue from './ObsidianStatue.json';
+import FrostWyrm from './FrostWyrm.json';
+// Mobs Night Elf
+import Wisp from './Wisp.json';
+
+// Buildings Undead
+import Necropolis from './Necropolis.json';
+import Ziggurat from './Ziggurat.json';
+import AltarOfDarkness from './AltarOfDarkness.json';
+import Crypt from './Crypt.json';
+import SacrificialPit from './SacrificialPit.json';
+import Graveyard from './Graveyard.json';
+import Slaughterhouse from './Slaughterhouse.json';
+import TempleOfTheDamned from './TempleOfTheDamned.json';
+import Boneyard from './Boneyard.json';
+import TombOfRelics from './TombOfRelics.json';
 
 // Types
 import { Unit } from '../bus/config/types';
 
 export const units: Array<Unit> = [
     // unitJsonMarker
-    FrostWyrm,
-    ObsidianStatue,
-    MeatWagon,
-    Necromancer,
-    Banshee,
-    Abomination,
-    Shade,
-    CryptFiend,
-    Ghoul,
-    Dreadlord,
-    Lich,
-    DeathKnight,
-    Shaman,
-    Acolyte,
-    Wisp,
-    Peon,
+
+    // Heroes Human
     Peasant,
+    // Heroes Orc
     Blademaster,
-    TaurenChieftain,
-    ShadowHunter,
     FarSeer,
-    Headhunter,
+    ShadowHunter,
+    TaurenChieftain,
+    // Heroes Undead
+    DeathKnight,
+    Lich,
+    Dreadlord,
+    CryptLord,
+    // Heroes Night Elt
+    Wisp,
+
+    // Mobs Orc
+    Peon,
     Grunt,
+    Headhunter,
+    Shaman,
+    // Mobs Undead
+    Acolyte,
+    Shade,
+    Ghoul,
+    Abomination,
+    MeatWagon,
+    CryptFiend,
+    Gargoyle,
+    Banshee,
+    Necromancer,
+    ObsidianStatue,
+    FrostWyrm,
+
+    // Buildings Undead
+    Necropolis,
+    Ziggurat,
+    AltarOfDarkness,
+    Crypt,
+    SacrificialPit,
+    Graveyard,
+    Slaughterhouse,
+    TempleOfTheDamned,
+    Boneyard,
+    TombOfRelics,
 ];
